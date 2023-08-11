c46220c0751b14160af6216c47af57d15e54fae4
gkmurombo <gkmurombo@gmail.com>
2023-08-11 09:31:20 +0200

    latest weather app

--- week4.js
+++ week4.js
@@ -22,14 +22,24 @@ function changeCity(event) {
   event.preventDefault();
   let entercity = document.querySelector("#citysearch");
   let h1 = document.querySelector("h1");
-  h1.innerHTML = entercity.value;
-}
+  let city = entercity.value;
+  h1.innerHTML = city;
+  let apiKey = "88724523008dc9e1be18f6eb6a959b67";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
+  axios.get(apiUrl).then(showWeather);
+}
 function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
   let humidity = Math.round(response.data.main.humidity);
   let wspeed = Math.round(response.data.wind.speed);
-
+  console.log(response.data);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let descr = response.data.weather[0].main;
+  let desc = document.querySelector("#descrip");
+  desc.innerHTML = descr;
   let temp = document.querySelector(".temperature");
   temp.innerHTML = temperature;
   let humi = document.querySelector("#hum");
@@ -37,18 +47,24 @@ function showWeather(response) {
   let wispeed = document.querySelector("#win");
   wispeed.innerHTML = `WindSpeed : ${wspeed} km/hr`;
 }
+function allIn(event) {
+  event.preventDefault();
 
-let apiKey = "88724523008dc9e1be18f6eb6a959b67";
-let units = "metric";
-let entercit = document.querySelector("#citysearch");
-let h1 = document.querySelector("h1");
-h1.innerHTML = entercit.value;
-let city = "cape town";
-console.log(city);
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  function currentloc(position) {
+    console.log(position);
 
-axios.get(apiUrl).then(showWeather);
+    let Apkey = "88724523008dc9e1be18f6eb6a959b67";
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${Apkey}&units=metric`;
+    axios.get(url).then(showWeather);
+  }
 
+  navigator.geolocation.getCurrentPosition(currentloc);
+}
+
+let currentbttn = document.querySelector("#currentb");
+currentbttn.addEventListener("click", allIn);
 let searchbutton = document.querySelector("#searchb");
 searchbutton.addEventListener("click", changeCity);
 console.log(currtime());
